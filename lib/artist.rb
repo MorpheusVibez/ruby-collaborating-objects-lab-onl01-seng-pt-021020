a51235155f52ee2d9f92ad3aa5c689f756431a5e
Andre Skeene <halodre24@gmail.com>
2020-03-07 21:33:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,10 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
     @@all << self
   end
 
@@ -12,7 +13,7 @@ class Artist
   end
 
   def add_song(song)
-  song.artist = self
+  @songs << song
   end
 
   def songs

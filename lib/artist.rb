c394aeae188653d423f1581e4adc7c7110211f4e
Andre Skeene <halodre24@gmail.com>
2020-03-07 21:54:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,23 +20,23 @@ class Artist
     @songs << song
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-
-  def self.find_or_create_by_name(artist_name)
-    artist = self.all.find {|artist| artist.name == artist_name}
-    if artist
-      artist
-    else
-      new_artist = self.new(artist_name)
-      new_artist
-    end
-  end
-
-  def print_songs
-    @songs.each do |song_name|
-      puts song_name.name
-    end
-  end
-end
+#   def songs
+#     Song.all.select {|song| song.artist == self}
+#   end
+#
+#   def self.find_or_create_by_name(artist_name)
+#     artist = self.all.find {|artist| artist.name == artist_name}
+#     if artist
+#       artist
+#     else
+#       new_artist = self.new(artist_name)
+#       new_artist
+#     end
+#   end
+#
+#   def print_songs
+#     @songs.each do |song_name|
+#       puts song_name.name
+#     end
+#   end
+# end

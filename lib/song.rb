1204434de6c98a354ce2dc0f873144ccf697283a
Andre Skeene <halodre24@gmail.com>
2020-03-08 00:28:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,11 +19,11 @@ class Song
     @@all
   end
 
-  def artist_name(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
-    self.artist
-  end
+  # def artist_name(name)
+  #   self.artist = Artist.find_or_create_by_name(name)
+  #   self.artist.add_song(self)
+  #   self.artist
+  # end
 
   def self.new_by_filename(filename)
     files = filename.split(" - ")

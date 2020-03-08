0743ac80b5de44b92eb89a5414ae8bf337ab9908
Andre Skeene <halodre24@gmail.com>
2020-03-08 00:39:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,9 @@ class Song
     @@all
   end
 
-  def artist_name(name)
+  def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)  
+    artist.add_song(self)
   end
 
   def self.new_by_filename(filename)
